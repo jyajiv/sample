d1c11efd8c0da2c7fd6fb060abacddb7b23fb14a
Vijay <vijay.jeyaraman@cognizant.com>
2020-05-26 14:25:58 +0530

    Git diff report for release/20.5.0 and release/20.5.0

--- devScripts/createDiffPR.js
+++ devScripts/createDiffPR.js
@@ -1,16 +1,16 @@
-const { exec } = require("child_process");
+const {exec} = require('child_process');
 const fs = require('fs');
 
 // const _ = require("lodash");
 
-let currentBranch = "";
-let previousBranch = "";
-let currentUserFork = "";
+let currentBranch = '';
+let previousBranch = '';
+let currentUserFork = '';
 
-const baseRepo = "praveenkumarc86/caribou";
-let pullRequestMessage = "Change report";
+const baseRepo = 'praveenkumarc86/caribou';
+let pullRequestMessage = 'Change report';
 
-createPR("release/20.5.0", "release/20.5.0");
+createPR('release/20.5.0', 'release/20.5.0');
 
 function getGitUserInfo() {
   exec(
@@ -20,9 +20,9 @@ function getGitUserInfo() {
         throw error;
       } else {
         previousBranch = output.trim();
-        console.log("previous Branch is ", previousBranch);
+        console.log('previous Branch is ', previousBranch);
       }
-    }
+    },
   );
 
   exec(
@@ -32,7 +32,7 @@ function getGitUserInfo() {
         throw error;
       } else {
         currentBranch = output.trim();
-        console.log("Current branch is ", currentBranch);
+        console.log('Current branch is ', currentBranch);
         exec(
           `git remote get-url origin | cut -d"/" -f 4`,
           (err, stdOut, stdIn) => {
@@ -40,13 +40,13 @@ function getGitUserInfo() {
               throw err;
             } else {
               currentUserFork = `${stdOut.trim()}/example-services`;
-              console.log("Current User is ", currentUserFork);
+              console.log('Current User is ', currentUserFork);
               getGitUserDetailsCallback();
             }
-          }
+          },
         );
       }
-    }
+    },
   );
 }
 
@@ -55,41 +55,41 @@ function createPR(sourceBranch, destinationBranch) {
   getGitUserInfo();
 }
 
-const gitPushCallback = (callback) => (err, stdOut, stdIn) => {
+const gitPushCallback = callback => (err, stdOut, stdIn) => {
   if (err) {
-  // if (!_.includes(_.get(err, "message", ""), "No staged files")) {
-      console.log(err);
-      throw err;
-   //  }
+    // if (!_.includes(_.get(err, "message", ""), "No staged files")) {
+    console.log(err);
+    throw err;
+    //  }
   }
-  console.log("Creating new Pull request ...");
+  console.log('Creating new Pull request ...');
   console.log(
-    `hub pull-request -f -b ${currentUserFork}:${previousBranch} -h ${currentUserFork}:${currentBranch} -m '${pullRequestMessage}'`
+    `hub pull-request -f -b ${currentUserFork}:${previousBranch} -h ${currentUserFork}:${currentBranch} -m '${pullRequestMessage}'`,
   );
   exec(
     `hub pull-request -f -b ${currentUserFork}:${previousBranch} -h ${currentUserFork}:${currentBranch} -m '${pullRequestMessage}'`,
     (error, output, input) => {
       if (error) {
-        console.log("Error creating PR - ", error);
+        console.log('Error creating PR - ', error);
       } else {
-        console.log("Pull request created successfully");
+        console.log('Pull request created successfully');
       }
-    }
+    },
   );
 };
 
 function getGitUserDetailsCallback() {
-  console.log("Fetch git repo details successfull");
+  console.log('Fetch git repo details successfull');
   console.log(
-    `git add . && git commit -m '${pullRequestMessage}' && git push origin HEAD:${currentBranch}`
+    `git add . && git commit -m '${pullRequestMessage}' && git push origin HEAD:${currentBranch}`,
   );
-  fs.writeFile("./test.txt", `this is the diff report${new Date()}`, (err) => {
-    console.log("File created", err);
+  fs.writeFile('./test.txt', `this is the diff report${new Date()}`, err => {
+    console.log('File created', err);
     exec(
       `git add . && git commit -m '${pullRequestMessage}' && git push origin HEAD:${currentBranch}`,
-      gitPushCallback()
+      gitPushCallback(),
     );
   });
 }
 
-module.exports = { createPR };
+module.exports = {createPR};

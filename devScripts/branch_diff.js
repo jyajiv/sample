60c2ef146fa72d6585a2455e957385102b8457fc
Vijay <vijay.jeyaraman@cognizant.com>
2020-05-27 11:24:40 +0530

    update branch_diff

--- devScripts/branch_diff.js
+++ devScripts/branch_diff.js
@@ -1,9 +1,9 @@
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 
 const getModuleNames = require('./getModuleName');
 const diffPR = require('./createDiffPR');
 
-const branches = process.argv.slice(2);
+let branches = process.argv.slice(2);
 
 const asyncCallback = callback => (err, stdout, stdin) => {
   if (err) {
@@ -34,33 +34,40 @@ const executeCommand = () => {
 if (branches.length === 2) {
   executeCommand(branches[0], branches[1]);
 } else {
-  console.log('\x1b[31m%s\x1b[0m: ', '****Please enter 2 branches for diff****');
+  console.log(
+    '\x1b[31m%s\x1b[0m: ',
+    '****Please enter 2 branches for diff****',
+  );
   console.log(
     '****  Missing one or both brach names. Hence comparring current branch with its parent. ****',
   );
+  branches = ['', ''];
   getBranchDetails();
 }
 
-const previousCallBack = (error, output, input) => {
+const currentBranchCallBack = (error, output, input) => {
   if (error) {
     throw error;
   } else {
-    branches[1] = output.trim();
-    console.log('Current branch is ', branches[1]);
+    branches[0] = output.trim();
+    console.log('previous Branch is ', output.trim());
     executeCommand(branches[0], branches[1]);
   }
 };
 
 function getBranchDetails() {
   exec(
-    `git show-branch | grep "*" | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -n1 | sed "s/.*\\[\\(.*\\)\\].*/\\1/" | sed "s/[\\^~].*//"`,
+    `git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3,4,5`,
     (error, output, input) => {
       if (error) {
         throw error;
       } else {
-        branches[0] = output.trim();
-        console.log('previous Branch is ', branches[0]);
-        exec(`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3,4,5`, previousCallBack);
+        branches[1] = output.trim();
+        console.log('Current branch is ', output.trim());
+        exec(
+          `git show-branch | grep "*" | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -n1 | sed "s/.*\\[\\(.*\\)\\].*/\\1/" | sed "s/[\\^~].*//"`,
+          currentBranchCallBack,
+        );
       }
     },
   );
